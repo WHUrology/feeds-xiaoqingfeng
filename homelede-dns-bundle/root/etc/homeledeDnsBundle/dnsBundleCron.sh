5c02afd98dbcae0374f407f93348c2d2ded0fb0c
WHUrology <107327908+WHUrology@users.noreply.github.com>
2024-01-14 10:43:05 +0800

    Delete homelede-dns-bundle directory

--- homelede-dns-bundle/root/etc/homeledeDnsBundle/dnsBundleCron.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-/bin/sh /etc/homeledeDnsBundle/daemon.sh
-sleep 30; /bin/sh /etc/homeledeDnsBundle/daemon.sh
--- homelede-dns-bundle/root/etc/homeledeDnsBundle/targetProcStates.sh
+++
@@ -1,4 +0,0 @@
-adBlockTargetState=1
-shuntDnsTargetState=1
-domesticDnsTargetState=1
-overseasDnsTargetState=1
